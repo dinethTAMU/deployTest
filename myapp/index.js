c1f51d59eed43ef10c58f1fe116b58555e537ffd
dinethG2 <47993609+dinethG2@users.noreply.github.com>
2025-03-25 23:14:14 -0500

    changing project structure

--- myapp/index.js
+++ myapp/index.js
@@ -1,15 +1,49 @@
-const http = require('http');
-const hostname = '127.0.0.1';
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
+// Create express app
+const app = express();
 const port = 3000;
 
-// Create a server object and pass an arrow function
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+// Create pool
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: {rejectUnauthorized: false}
 });
 
-// Let the server listen on 127.0.0.1:3000
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Add process hook to shutdown pool
+process.on('SIGINT', function() {
+    pool.end();
+    console.log('Application successfully shutdown');
+    process.exit(0);
+});
+	 	 	 	
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    const data = {name: 'Mario'};
+    res.render('index', data);
+});
+
+app.get('/user', (req, res) => {
+    teammembers = []
+    pool
+        .query('SELECT * FROM teammembers;')
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++){
+                teammembers.push(query_res.rows[i]);
+            }
+            const data = {teammembers: teammembers};
+            console.log(teammembers);
+            res.render('user', data);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
